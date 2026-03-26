042fe5d0cabae84471a6fd8e7943ba18579aa509
Antônio Rafael <antonio.rafael0415@gmail.com>
2026-03-26 19:07:24 -0300

    Finished challenge

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-git
+
+let heroName = "Rafael";
+let heroXP = 8455;
+let heroLevel;
+
+switch (true) {
+
+  case heroXP <= 1000:
+    heroLevel = "Ferro";
+    break;
+
+  case heroXP <= 2000:
+    heroLevel = "Bronze";
+    break;
+
+  case heroXP <= 5000:
+    heroLevel = "Prata";
+    break;
+
+  case heroXP <= 7000:
+    heroLevel = "Ouro";
+    break;
+
+  case heroXP <= 8000:
+    heroLevel = "Platina";
+    break;
+
+  case heroXP <= 9000:
+    heroLevel = "Ascendente";
+    break;
+
+  case heroXP <= 10000:
+    heroLevel = "Imortal";
+    break;
+
+  case heroXP > 10000:
+    heroLevel = "Radiante";
+    break;
+}
+
+console.log(`O Herói de nome ${heroName} está no nível de ${heroLevel}`);
